f8e1c319772e05e13308c57eaabfe9385bc1670d
Djimmka <dimsti94@gmail.com>
2022-11-15 08:15:09 +0300

    2.3.1 ready

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,66 @@
+package web.controller;
+
+//import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+//import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import web.models.User;
+import web.servise.UserService;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String index(Model model){
+        //userService.gen5Users();
+        model.addAttribute("users", userService.findAll());
+        return "users/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userService.findById(id));
+        return "users/show";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user) {return "users/new";}
+
+    @PostMapping
+    public String create(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") long id) {
+        model.addAttribute("user", userService.findById(id));
+        return "users/edit";
+    }
+
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") User user,
+                         @PathVariable("id") long id) {
+        userService.update(id,user);
+        return "redirect:/users";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") long id) {
+        userService.delete(id);
+        return "redirect:/users";
+    }
+
+
+}
