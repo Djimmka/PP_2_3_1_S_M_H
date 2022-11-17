385700af98618e66b4f4c18bf226d7055c18614f
Djimmka <dimsti94@gmail.com>
2022-11-17 17:15:33 +0300

    2.3.1 добавлен интерфейс сервиса

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import web.models.User;
 import web.servise.UserService;
+import web.servise.UserServiceImp;
 
 @Controller
 @RequestMapping("/users")
